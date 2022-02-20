5cb217e3141009ec486ecd24c49d05b66d72ae6b
godhj <godhj@unist.ac.kr>
2022-02-20 19:32:02 +0900

    add args states

--- train.py
+++ train.py
@@ -9,17 +9,18 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu,True)
 
 parser = argparse.ArgumentParser('Train MobilViT AutoEncoder')
-parser.add_argument("--ep", default=10, type=int,help="Epochs")
+parser.add_argument("--ep", default=5, type=int,help="Epochs")
 parser.add_argument("--bs", default=8, type=int,help="Batch Size")
 #parser.add_argument("--arch", default='S', type=str,help="Architecture: [S, XS, XSS]")
 parser.add_argument("--data", default='NYUv2')
 parser.add_argument("--name", default='AutoEncoder')
 parser.add_argument("--alpha", type=float, default=0.5)
+parser.add_argument("--states", type=int, default=64)
 args = parser.parse_args()
 
 def main():
     
-    autoencoder = AutoEncoder(classes=64).model(input_shape=(256,256,1))
+    autoencoder = AutoEncoder(classes=args.states).model(input_shape=(256,256,1))
     print(autoencoder.summary())
     print(f"Epochs: {args.ep}\nBatch Size: {args.bs}\nAlpha: {float(args.alpha)} \n")
     trainer = Trainer(autoencoder, epochs=args.ep, batch_size=args.bs, size= 256, alpha= args.alpha, DEBUG=False)
