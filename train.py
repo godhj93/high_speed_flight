a7a000c014d6fab4bddbf835c45f69c22eb714b7
godhj <godhj@unist.ac.kr>
2022-02-20 19:30:34 +0900

    add alpha

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import tensorflow as tf
 from utils.op import Trainer
 import argparse
@@ -13,13 +14,14 @@ parser.add_argument("--bs", default=8, type=int,help="Batch Size")
 #parser.add_argument("--arch", default='S', type=str,help="Architecture: [S, XS, XSS]")
 parser.add_argument("--data", default='NYUv2')
 parser.add_argument("--name", default='AutoEncoder')
-parser.add_argument("--alpha", default='Loss alpha')
+parser.add_argument("--alpha", type=float, default=0.5)
 args = parser.parse_args()
 
 def main():
-
+    
     autoencoder = AutoEncoder(classes=64).model(input_shape=(256,256,1))
     print(autoencoder.summary())
+    print(f"Epochs: {args.ep}\nBatch Size: {args.bs}\nAlpha: {float(args.alpha)} \n")
     trainer = Trainer(autoencoder, epochs=args.ep, batch_size=args.bs, size= 256, alpha= args.alpha, DEBUG=False)
     trainer.train()
     trainer.save_model(name=args.name)
