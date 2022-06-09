5915def7b33636db267fc978de33a0e3691bbee1
godhj <godhj@unist.ac.kr>
2022-06-10 03:31:48 +0900

    almost done

--- utils/nets/MobileViT.py
+++ utils/nets/MobileViT.py
@@ -1,15 +1,17 @@
+from cmath import log
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 class MobileViT(nn.Module):
     
-    def __init__(self,arch, input_shape, classes=64):
+    def __init__(self,arch, input_shape, classes=1000):
         super(MobileViT, self).__init__()
 
         self.arch = arch
         self.input_shape = input_shape
-
-        self.conv1 = nn.Conv2d(in_channels=input_shape[1], out_channels=16, kernel_size=3, stride=2)
+        print(self.input_shape)
+        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=16, kernel_size=3, stride=2, bias=False)
         self.zeropad = nn.ZeroPad2d(padding=(1,1,1,1))
         self.MV1 = InvertedRedisual(in_channels=16, out_channels=32, strides=1)
 
@@ -17,32 +19,54 @@ class MobileViT(nn.Module):
         self.MV3 = InvertedRedisual(in_channels=64, out_channels=64, strides=1)
 
         self.MV4 = InvertedRedisual(in_channels=64, out_channels=96, strides=2)
-        #MobileViT block
+        self.ViT1 = MobileViT_Block(in_channels=96, dimension=144, num_of_encoder=2)
 
         self.MV5 = InvertedRedisual(in_channels=96, out_channels=128, strides=2)
-        #MobileViT block
+        self.ViT2 = MobileViT_Block(in_channels=128, dimension=192, num_of_encoder=4)
         
         self.MV6 = InvertedRedisual(in_channels=128, out_channels=160, strides=2)
-        #MobileViT block
-        self.conv2 = nn.Conv2d(in_channels=160, out_channels=640, kernel_size=1, stride=1)
+        self.ViT3 = MobileViT_Block(in_channels=160, dimension=240, num_of_encoder=3)
 
-        self.global_pool = nn.AvgPool2d(kernel_size=7)
-        self.logits = nn.Linear(in_features=640, out_features=classes)
+        self.conv2 = nn.Conv2d(in_channels=160, out_channels=640, kernel_size=1, stride=1, bias=False)
+
+        self.global_pool = nn.AvgPool2d(kernel_size=8)
+        self.logits = nn.Linear(in_features=640, out_features=classes, bias=False)
         self.relu6 = nn.ReLU6()
+        self.swish = Swish()
 
     def forward(self, x):
 
-        y = (self.conv1(self.zeropad(x)))
+        y = self.swish((self.conv1(self.zeropad(x))))
+        print(y.shape)
         y = self.MV1(y)
+        print(y.shape)
         y = self.MV2(y)
+        print(y.shape)
         y = self.MV3(y)
+        print(y.shape)
         y = self.MV4(y)
+        print(y.shape)
+        y = self.ViT1(y)
+        print(y.shape)
+        
         y = self.MV5(y)
+        print(y.shape)
+        y = self.ViT2(y)
+        print(y.shape)
+
         y = self.MV6(y)
-        y = self.conv2(y)
+        print(y.shape)
+        y = self.ViT3(y)
+        print(y.shape)
+
+        y = self.swish(self.conv2(y))
+        print(y.shape)
         y = self.global_pool(y)
+        print(y.shape)
         y = y.squeeze()
-        logits = self.relu6(self.logits(y))
+        print(y.shape)
+        logits = self.swish(self.logits(y))
+        print(logits.shape)
 
         return logits
 
@@ -55,23 +79,25 @@ class InvertedRedisual(nn.Module):
         self.out_channels = out_channels
         self.strides=strides
 
-        self.pw_conv1 = nn.Conv2d(in_channels=self.in_channels, out_channels=self.in_channels*6, padding='same', stride=1, kernel_size=1)
+        self.pw_conv1 = nn.Conv2d(in_channels=self.in_channels, out_channels=self.in_channels*6, padding='same', stride=1, kernel_size=1, bias=False)
 
         if self.strides == 2:
-            self.dw_conv1 = nn.Conv2d(in_channels=self.in_channels*6, out_channels=self.in_channels*6, stride=self.strides, kernel_size=3, groups=self.in_channels)
+            self.dw_conv1 = nn.Conv2d(in_channels=self.in_channels*6, out_channels=self.in_channels*6, stride=self.strides, kernel_size=3, groups=self.in_channels, bias=False)
         else:
-            self.dw_conv1 = nn.Conv2d(in_channels=self.in_channels*6, out_channels=self.in_channels*6, stride=self.strides, padding='same', kernel_size=3, groups=self.in_channels)
+            self.dw_conv1 = nn.Conv2d(in_channels=self.in_channels*6, out_channels=self.in_channels*6, stride=self.strides, padding='same', kernel_size=3, groups=self.in_channels, bias=False)
 
-        self.pw_conv2 = nn.Conv2d(in_channels=self.in_channels*6, out_channels=self.out_channels, padding='same', stride=1, kernel_size=1)
+        self.pw_conv2 = nn.Conv2d(in_channels=self.in_channels*6, out_channels=self.out_channels, padding='same', stride=1, kernel_size=1, bias=False)
 
-        self.relu6 = nn.ReLU6()        
+        self.relu6 = nn.ReLU6()   
+        self.swish = Swish()     
         self.zeropad = nn.ZeroPad2d(padding=(1,1,1,1))
+
     def forward(self, x):
 
-        y = self.relu6(self.pw_conv1(x))
+        y = self.swish(self.pw_conv1(x))
         if self.strides == 2:
             y = self.zeropad(y)
-        y = self.relu6(self.dw_conv1((y)))
+        y = self.swish(self.dw_conv1((y)))
         y = self.pw_conv2(y)
         
         if self.strides == 1 and (x.shape == y.shape):
@@ -79,14 +105,9 @@ class InvertedRedisual(nn.Module):
         else:
             return y
         
-
-
-
-
-
 class MobileViT_Block(nn.Module):
 
-    def __init__(self, in_channels, dimension ):
+    def __init__(self, in_channels, dimension, num_of_encoder ):
         super(MobileViT_Block, self).__init__()
 
         self.in_channels = in_channels
@@ -95,8 +116,8 @@ class MobileViT_Block(nn.Module):
         h,w = 2,2
 
         self.P = h*w
-        self.conv1_local_rep = nn.Conv2d(in_channels= self.in_channels, out_channels= self.in_channels, kernel_size=3, stride=1, padding='same')
-        self.conv2_local_rep = nn.Conv2d(in_channels= self.in_channels, out_channels= self.dimension, kernel_size=1, stride=1, padding='same')
+        self.conv1_local_rep = nn.Conv2d(in_channels= self.in_channels, out_channels= self.in_channels, kernel_size=3, stride=1, padding='same', bias=False)
+        self.conv2_local_rep = nn.Conv2d(in_channels= self.in_channels, out_channels= self.dimension, kernel_size=1, stride=1, padding='same', bias=False)
 
         #Unfold
         #Extract Patches
@@ -104,65 +125,65 @@ class MobileViT_Block(nn.Module):
             #Flatten Patches
 
         #Transformer Encoder
-
+        self.encoder = Transformer_Encoder(input_dim=dimension, num_of_encoder=num_of_encoder)
         #Fusion
-        self.conv1_fusion = nn.Conv2d(in_channels=self.dimension, out_channels=self.in_channels, kernel_size=1,stride=1, padding='same')
-        self.conv2_fusion = nn.Conv2d(in_channels=self.in_channels*2, out_channels=self.in_channels, kernel_size=3,stride=1, padding='same')
+        self.conv1_fusion = nn.Conv2d(in_channels=self.dimension, out_channels=self.in_channels, kernel_size=1,stride=1, padding='same', bias=False)
+        self.conv2_fusion = nn.Conv2d(in_channels=self.in_channels*2, out_channels=self.in_channels, kernel_size=3,stride=1, padding='same', bias=False)
 
         self.swish = Swish()
         
     def forward(self, x):
-
+        
         N,C,H,W = x.shape
-        print(x.shape)
+        
         self.fold = torch.nn.Fold(output_size=(H,W), kernel_size=2, stride=2)
         
         #Local reprentations.
-        y = self.conv1_local_rep(x)        
-        y = self.conv2_local_rep(y)
-        print(y.shape) 
+        y = self.swish(self.conv1_local_rep(x))
+        y = self.swish(self.conv2_local_rep(y))
         #Transformers as Convolutions
             #Unfold
         y = self.extract_patches(y)
         c,h,w = y.shape
-        print(y.shape)        
-        y = y.reshape(self.dimension, -1, self.P)
-        print(y.shape)        
+        y = y.reshape(self.dimension, self.P, -1)
+        y = y.permute(2,1,0)
             #Transformer encoder
-        
+        y = self.encoder(y)
             #Fold
         y = y.reshape(c,h,w)
-        print(y.shape)
+        
         y = self.fold(y)
-        print(y.shape)
-        y = self.conv1_fusion(y)
-        print(y.shape)
+        
+        y = self.swish(self.conv1_fusion(y))
+        
         y = torch.cat([x,y],axis=1)
-        print(y.shape)
-        y = self.conv2_fusion(y)
-        print(y.shape)
+        
+        y = self.swish(self.conv2_fusion(y))
+        
         return y
 
     
 class Swish(nn.Module):
 
     def __init__(self):
-        super().__init__()
+        super(Swish, self).__init__()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         return x*self.sigmoid(x)
-
-
-class Swish(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        return x*self.sigmoid(x)
-
 
 
 # Transformer Encoder
+class Transformer_Encoder(nn.Module):
+    
+    def __init__(self, input_dim, num_of_encoder):
+        super(Transformer_Encoder, self).__init__()
+
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=input_dim, nhead=4, dim_feedforward=input_dim)
+        self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_of_encoder)
+        
+    def forward(self, x):
+
+        return self.encoder(x)
+
+     
